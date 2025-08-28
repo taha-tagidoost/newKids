6fc02d9722c133deec05f35045bd0893a7ae301c
pooyan <thepooyan@gmail.com>
2025-08-28 16:46:56 +0330

    nothing!

--- Scripts/js/kids.js
+++ Scripts/js/kids.js
@@ -3,7 +3,11 @@ let consultModal = document.getElementById("consultModal")
 if (consultBtn && consultModal) {
   consultBtn.addEventListener("click", () => {
     consultModal.style = ""
-    callModal.fitContent(consultModal)
+    let close = consultModal.querySelector("i")
+    callModal.custom("fitContent", (showModal, closeModal) => {
+      showModal(consultModal)
+      close.addEventListener("click", closeModal)
+    })
   })
   consultModal.querySelector("button").addEventListener("click", () => {
     validateSection(consultModal).then(() => alert("با موفقیت انجام شد!"))
