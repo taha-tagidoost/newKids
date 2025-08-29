3196ec5e456873b6e621806803fadcd0fdeece1c
pooyan <thepooyan@gmail.com>
2025-08-29 10:47:31 +0330

    nothing!

--- Scripts/js/kids.js
+++ Scripts/js/kids.js
@@ -1,15 +1,23 @@
-let consultBtn = document.getElementById("getConsult")
-let consultModal = document.getElementById("consultModal")
-if (consultBtn && consultModal) {
-  consultBtn.addEventListener("click", () => {
-    consultModal.style = ""
-    let close = consultModal.querySelector("i")
-    callModal.custom("fitContent", (showModal, closeModal) => {
-      showModal(consultModal)
-      close.addEventListener("click", closeModal)
+$(() => {
+  let consultBtn = document.getElementById("getConsult")
+  let consultModal = document.getElementById("consultModal")
+  if (consultBtn && consultModal) {
+    consultBtn.addEventListener("click", () => {
+      consultModal.style = ""
+      let close = consultModal.querySelector("i")
+      callModal.custom("fitContent", (showModal, closeModal) => {
+        showModal(consultModal)
+        close.addEventListener("click", closeModal)
+      })
     })
-  })
-  consultModal.querySelector("button").addEventListener("click", () => {
-    validateSection(consultModal).then(() => alert("با موفقیت انجام شد!"))
-  })
-}
+    consultModal.querySelector("button").addEventListener("click", () => {
+      validateSection(consultModal).then(() => alert("با موفقیت انجام شد!"))
+    })
+  }
+
+  let resultModal = document.getElementById("testResaultModal");
+  if (resultModal) {
+    resultModal.style = ""
+    callModal.fitContent(resultModal)
+  }
+})
